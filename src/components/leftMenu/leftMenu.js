e21f706c44c48306ccc82f32408f48f998543a1c
Alan <36890792+Alan-Koibagarov@users.noreply.github.com>
2021-01-15 16:33:25 +0600

    leftMenu update

--- src/components/leftMenu/leftMenu.js
+++ src/components/leftMenu/leftMenu.js
@@ -1,12 +1,27 @@
 import classes from './leftMenu.module.css'
 function LeftMenu () {
+    let items = ['Заметка 1', 'Заметка 2', "Заметка на заметке 2"]
+    let deletedItems = [1,2,3]
     return (
         <div className={classes.left}>
             <div className={classes.header}>
+             <span>Logo</span>
              <button className={classes.btn}>Add Button</button>
             </div>
-            <div className={classes.notes}>2</div>
-            <div className={classes.notes}>3</div>
+            <div className={classes.notes}>
+            {items.map(item => {
+                return(
+                    <div key={item} className={classes.note}>{item}</div>
+                )
+            })}
+            </div>
+            <div style={{borderTop: '1px solid #fff'}} className={classes.notes + '' + '123'}>
+            {deletedItems.map(item => {
+                return(
+                    <div key={item} className={classes.note}>{item}</div>
+                )
+            })}
+            </div>
         </div>
     )
 }
